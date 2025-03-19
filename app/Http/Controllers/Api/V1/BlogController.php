38b1a00e334db973496ef0a5fe746e9f8715c18f
ALFAFA <gasan1288@gmail.com>
2025-03-19 08:27:25 +0400

    `Added try-catch blocks to handle exceptions in BlogController methods`

--- app/Http/Controllers/Api/V1/BlogController.php
+++ app/Http/Controllers/Api/V1/BlogController.php
@@ -7,49 +7,71 @@ use App\Http\Requests\Api\V1\BlogRequest;
 use App\Http\Resources\Api\V1\BlogResource;
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use Exception;
 use Illuminate\Support\Str;
 class BlogController extends Controller
 {
     public function index()
     {
-        return $this->api()->success(BlogResource::collection(Blog::latest()->paginate(10)));
+        try {
+            return $this->api()->success(BlogResource::collection(Blog::latest()->paginate(10)));
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
     }
 
     public function store(BlogRequest $request)
     {
-        $blog = Blog::create([
-            'title' => $request->title,
-            'content' => $request->content,
-            'short_description' => $request->short_description,
-            'slug' => Str::slug($request->title),
-            'image' => $request->image,
-            'user_id' => $request->user_id,
-        ]);
+        try {
+            $blog = Blog::create([
+                'title' => $request->title,
+                'content' => $request->content,
+                'short_description' => $request->short_description,
+                'slug' => Str::slug($request->title),
+                'image' => $request->image,
+                'user_id' => $request->user_id,
+            ]);
 
-        return $this->api()->created(new BlogResource($blog), "Blog created successfully");
+            return $this->api()->created(new BlogResource($blog), "Blog created successfully");
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
     }
 
-    public function show(Blog $blog)
+    public function show($id)
     {
-        return $this->api()->success(new BlogResource($blog));
+        try {
+            $blog = Blog::findOrFail($id);
+            return $this->api()->success(new BlogResource($blog));
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
     }
 
     public function update(BlogRequest $request, Blog $blog)
     {
-        $blog->update([
-            'title' => $request->title,
-            'content' => $request->content,
-            'short_description' => $request->short_description,
-            'slug' => Str::slug($request->title),
-            'image' => $request->image,
-        ]);
+        try {
+            $blog->update([
+                'title' => $request->title,
+                'content' => $request->content,
+                'short_description' => $request->short_description,
+                'slug' => Str::slug($request->title),
+                'image' => $request->image,
+            ]);
 
-        return $this->api()->success(new BlogResource($blog), "Blog Updated Successfully");
+            return $this->api()->success(new BlogResource($blog), "Blog Updated Successfully");
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
     }
 
     public function destroy(Blog $blog)
     {
-        $blog->delete();
-        return $this->api()->success([], 'Blog deleted successfully');
+        try {
+            $blog->delete();
+            return $this->api()->success([], 'Blog deleted successfully');
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
     }
 }
