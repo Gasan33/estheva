ab314fce101cf694f1fc134410a5cf2c92ed19f6
ALFAFA <gasan1288@gmail.com>
2025-04-17 16:57:09 +0400

    `Refactor Stripe API key usage in PaymentsController to use config services`

--- app/Http/Controllers/Api/V1/PaymentsController.php
+++ app/Http/Controllers/Api/V1/PaymentsController.php
@@ -20,7 +20,8 @@ class PaymentsController extends Controller
         try {
             $validatedData = $request->validated();
             Stripe::setApiKey(env('STRIPE_SECRET'));
-            dd(env('STRIPE_SECRET'));
+            // dd(env('STRIPE_SECRET'));
+            dd(config('services.stripe.secret'));
             $paymentIntent = PaymentIntent::create([
                 'amount' => $validatedData['amount'] * 100,
                 'currency' => 'aed',
